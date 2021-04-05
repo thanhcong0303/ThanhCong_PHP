c0f0802e924b5b0ffa0e61c0a39ad8a640fc6e76
thanhcong0303 <67908408+thanhcong0303@users.noreply.github.com>
2021-04-05 18:08:31 +0700

    b

---
+++ Session9_JS/ex1.js
@@ -0,0 +1,15 @@
+function testExternal() {
+  document.getElementById("div2").innerHTML= "Hello World";
+
+}
+function div3()
+{
+  document.getElementById("div3").innerHTML="Đây là nội dung mới";
+}
+function div3_2()
+{
+  document.getElementById("div3").innerHTML="Đây là nội dung đầu tiên";
+}
+function changeImage(){
+  document.getElementById("change_image").src="anh3d.jpg"
+}
